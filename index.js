b52279f68a915db0a571544cd18d21532f59e3b2
AtefMohammed <wmhmdk034@gmail.com>
2026-02-16 12:54:28 +0200

    mm

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-salla.onReady(()=>{
+salla.onReady(() => {
+    CreateModal();
+})
 
+
+
+function CreateModal() {
     const modal = document.createElement("salla-modal");
     modal.id = "subscribe-modal";
     document.body.appendChild(modal);
 
     const button = document.createElement("button");
     button.textContent = "Open Modal";
-
     button.setAttribute("data-modal-id", "subscribe-modal");
+
     modal.open();
-
-
     document.body.appendChild(button);
-})
-
-
-
+}
