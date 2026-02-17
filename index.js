cdb5132da0a703f9ad7fcc826f614900f1d7acdd
AtefMohammed <wmhmdk034@gmail.com>
2026-02-17 09:44:38 +0200

    dd

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ function CopyPromotionCode(params) {
             // Reset after 2 seconds
             setTimeout(() => {
                 copyBtn.classList.remove("active");
-                copyBtn.textContent = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
                     </svg>`;
             }, 2000);
@@ -330,8 +330,9 @@ function injectStyle() {
             text-decoration: underline;
         }
         .copy-box {
-            display: inline-flex;
+            display: flex;
             align-items: center;
+            justify-content:space-between;
             gap: 8px;
             padding: 8px 12px;
             border: 1px dashed #3cb646;
