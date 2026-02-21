951810af3534910767b801f319b1e9c8ac2967be
Atef-mohamed <wmhmdk034@gmail.com>
2026-02-21 11:56:07 +0200

    mm

--- index.js
+++ index.js
@@ -1,73 +1,95 @@
-
 salla.onReady(() => {
+  if (
+    window.abqarino_popup_var.show_in_home |
+    window.abqarino_popup_var.show_in_category |
+    window.abqarino_popup_var.show_in_cart
+  ) {
     CreateModal();
 
     injectStyle();
 
     CLoseModalButton();
-    handleModalPrimaryBtn()
+    handleModalPrimaryBtn();
     CopyPromotionCode();
 
-    getProduct().then(products => {
-        renderProducts(products);
+    getProduct().then((products) => {
+      renderProducts(products);
     });
+  }
 });
 
 // foramt date
 function formatDate(date) {
-    let d;
-    if (typeof date === "number") {
-        if (date < 10000000000) {
-            d = new Date(date * 1000);
-        } else {
-            d = new Date(date);
-        }
-    } else if (typeof date === "string") {
-        d = new Date(date);
+  let d;
+  if (typeof date === "number") {
+    if (date < 10000000000) {
+      d = new Date(date * 1000);
     } else {
-        d = new Date(date);
+      d = new Date(date);
     }
-    const formatted = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-    return formatted;
+  } else if (typeof date === "string") {
+    d = new Date(date);
+  } else {
+    d = new Date(date);
+  }
+  const formatted = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+  return formatted;
 }
 
 function CreateModal() {
+  const data = window.abqarino_popup_var;
 
-    const data = window.abqarino_popup_var;
+  if (!data) {
+    return;
+  }
 
-    if (!data) {
-        return;
-    }
+  modal = document.createElement("salla-modal");
+  modal.id = "subscribe-modal";
 
-    modal = document.createElement("salla-modal");
-    modal.id = "subscribe-modal";
-
-    modal.innerHTML = `
+  modal.innerHTML = `
         <div class="modal-h" style="padding:20px; text-align:center; background:${data.bg_color};display:flex; flex-direction:column; justify-content:center; align-items:center; border-radius:${data.raduis_border}px;">
         <div class="content-top-side"  >
-            ${data.brand_logo ? `
+            ${
+              data.brand_logo
+                ? `
             <img src="${data.brand_logo}" style="    border-radius: 12px;
                 object-fit: cover;
                 aspect-ratio: 1 / 1;" />
-            `: ``}
+            `
+                : ``
+            }
             
-            <div class="content" style="display: flex;flex-direction: column; ${!data.brand_logo ? 'align-items: center; text-align: center;' : ''}">
-                ${data.brand_description ? `
+            <div class="content" style="display: flex;flex-direction: column; ${!data.brand_logo ? "align-items: center; text-align: center;" : ""}">
+                ${
+                  data.brand_description
+                    ? `
                 <p style="color:var(--color-primary); font-size: 33px;font-weight: 700;padding: 20px;">
                     ${data.brand_description}
                 </p>
-                `: ``}
-                ${data.brand_description2 ? `
+                `
+                    : ``
+                }
+                ${
+                  data.brand_description2
+                    ? `
                 <p style="color:var(--color-primary);opacity:0.6; font-size: 16px;padding: 4px;">
                     ${data.brand_description2}
                 </p>
-                `: ``}
+                `
+                    : ``
+                }
 
-                ${data.discount_time ? `
+                ${
+                  data.discount_time
+                    ? `
                     <salla-count-down date="${formatDate(data.discount_time)}" end-of-day="true" boxed="true" labeled="true"></salla-count-down>
-                `: ``}
+                `
+                    : ``
+                }
                 
-                 ${data.procode_txt ? `
+                 ${
+                   data.procode_txt
+                     ? `
                         <div class="copy-box">
                             <p>نسخ كود الخصم</p>
                             <div id="copyText">${data.procode_txt}</div>
@@ -77,95 +99,119 @@ function CreateModal() {
                                 </svg>
                             </button>
                         </div>
-                `: ``}
+                `
+                     : ``
+                 }
 
-             ${data.txt_btn_ok ? `
+             ${
+               data.txt_btn_ok
+                 ? `
                     <button class="ok-modal-btn" style="background: var(--color-primary); color:var(--color-primary-reverse); padding:10px 20px; border:none; border-radius:8px; cursor:pointer;">
                         ${data.txt_btn_ok}
                     </button>
-                    `: ``}
+                    `
+                 : ``
+             }
 
-                ${data.txt_btn_cancel ? `
+                ${
+                  data.txt_btn_cancel
+                    ? `
                 <button class="close-modal-btn" style="background:transparent; border:none; text-decoration: underline; cursor:pointer;">
                     ${data.txt_btn_cancel}
                 </button>
-                `: ``}
+                `
+                    : ``
+                }
 
             </div>
         </div>
-           ${data.dropdown_list ? `
+           ${
+             data.dropdown_list
+               ? `
                 <div id="products-container" class="products-container">
                 </div>
-                `: ``}
+                `
+               : ``
+           }
         </div>
     `;
 
-    document.body.appendChild(modal);
-    modal.open();
+  document.body.appendChild(modal);
+  modal.open();
 }
 
 // get product
 function getProduct() {
-    const raw = window.abqarino_popup_var.dropdown_list;
+  const raw = window.abqarino_popup_var.dropdown_list;
 
-    const productIds = Array.isArray(raw) ? raw : JSON.parse(raw);
+  const productIds = Array.isArray(raw) ? raw : JSON.parse(raw);
 
-    const requests = productIds.map(id =>
-        salla.product.getDetails(id, ["brand", "category"]).then(response => {
-            return response.data || response;
-        })
-    );
+  const requests = productIds.map((id) =>
+    salla.product.getDetails(id, ["brand", "category"]).then((response) => {
+      return response.data || response;
+    }),
+  );
 
-    return Promise.all(requests);
+  return Promise.all(requests);
 }
 
 // render products
 function renderProducts(products) {
+  const container = document.querySelector("#products-container");
+  if (!container) return;
 
-    const container = document.querySelector("#products-container");
-    if (!container) return;
+  container.innerHTML = "";
 
-    container.innerHTML = "";
+  if (products.length === 1) {
+    container.style.width = "auto";
+    container.style.maxWidth = "600px";
+    container.style.margin = "20px auto";
+  } else {
+    container.style.width = "100%";
+    container.style.margin = "20px 0";
+  }
 
-    if (products.length === 1) {
-        container.style.width = "auto";
-        container.style.maxWidth = "600px";
-        container.style.margin = "20px auto";
-    } else {
-        container.style.width = "100%";
-        container.style.margin = "20px 0";
-    }
+  if (products.length === 1) {
+    products.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card");
 
-    if (products.length === 1) {
-        products.forEach(product => {
-
-            const productCard = document.createElement("div");
-            productCard.classList.add("product-card");
-
-            productCard.innerHTML = `
+      productCard.innerHTML = `
             <div class="product-img">
-                ${product.image ? `
+                ${
+                  product.image
+                    ? `
                     <img src="${product.image.url}" 
                          alt="${product.name}"
                          style="max-width:100%;">
-                ` : ``}
+                `
+                    : ``
+                }
             </div>
 
             <div class="product-content">
                 <h3 class="product-title">${product.name || ""}</h3>
 
                 <p class="prices">
-                    ${product.sale_price ? `
+                    ${
+                      product.sale_price
+                        ? `
                         <span class="sale_price">
                             ${product.sale_price} ${product.currency || ""}
                         </span>
-                    ` : ``}
+                    `
+                        : ``
+                    }
 
-                    ${product.regular_price ? `
+                    ${
+                      product.regular_price
+                        ? `
                         <span class="discount_price">
                             ${product.regular_price} ${product.currency || ""}
                         </span>
-                    ` : ``}
+                    `
+                        : ``
+                    }
                     </p>
                     <div class="product-buttons">
                         <salla-add-product-button 
@@ -178,75 +224,90 @@ function renderProducts(products) {
 
             </div>
         `;
-            container.appendChild(productCard);
-        });
-    }
-    else {
-       //  Slider config
-        const sliderConfig = {
-            autoHeight: true,
-            loop: true,
-            keyboard: { enabled: true },
-            grabCursor: true,
-            speed: 500,
-            slidesPerView: 1,
-            spaceBetween: 10,
+      container.appendChild(productCard);
+    });
+  } else {
+    //  Slider config
+    const sliderConfig = {
+      autoHeight: true,
+      loop: true,
+      keyboard: { enabled: true },
+      grabCursor: true,
+      speed: 500,
+      slidesPerView: 1,
+      spaceBetween: 10,
 
-            breakpoints: {
-                768: {
-                    slidesPerView: 1,
-                    spaceBetween: 20
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 10
-                }
-            }
-        };
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+      },
+    };
 
-        // Create slider wrapper
-        container.innerHTML = `
+    // Create slider wrapper
+    container.innerHTML = `
         <salla-slider id="products-slider" show-controls="true" slider-config='${JSON.stringify(sliderConfig)}'>
             <div slot="items" class="products-slides"></div>
         </salla-slider>
     `;
 
-        //  Get slides container
-        const slidesContainer = container.querySelector(".products-slides");
+    //  Get slides container
+    const slidesContainer = container.querySelector(".products-slides");
 
-        // Loop products and add to slider
-        products.forEach(product => {
-            const productCard = document.createElement("div");
-            productCard.classList.add("product-card", "swiper-slide");
+    // Loop products and add to slider
+    products.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card", "swiper-slide");
 
-            productCard.innerHTML = `
+      productCard.innerHTML = `
             <div class="product-img">
-                ${product.image ? `
+                ${
+                  product.image
+                    ? `
                     <img src="${product.image.url}" 
                          alt="${product.name}"
                          style="max-width:100%;">
-                ` : ""}
+                `
+                    : ""
+                }
             </div>
 
             <div class="product-content">
                 <h3 class="product-title">${product.name || ""}</h3>
 
-                ${product.brand?.name ? `
+                ${
+                  product.brand?.name
+                    ? `
                     <p class="brand-name">${product.brand.name}</p>
-                ` : ""}
+                `
+                    : ""
+                }
                     <div class="product-content-bottom">
                         <p class="prices">
-                            ${product.sale_price ? `
+                            ${
+                              product.sale_price
+                                ? `
                                 <span class="sale_price">
                                     ${product.sale_price} ${product.currency || ""}
                                 </span>
-                            ` : ""}
+                            `
+                                : ""
+                            }
 
-                            ${product.regular_price ? `
+                            ${
+                              product.regular_price
+                                ? `
                                 <span class="discount_price">
                                     ${product.regular_price} ${product.currency || ""}
                                 </span>
-                            ` : ""}
+                            `
+                                : ""
+                            }
                         
                         </p>
                         <div class="product-buttons">
@@ -259,86 +320,83 @@ function renderProducts(products) {
             </div>
         `;
 
-            // Cancel button
-            const cancelBtn = productCard.querySelector(".cancel-btn");
-            if (cancelBtn) {
-                cancelBtn.addEventListener("click", () => {
-                    productCard.style.display = "none";
-                });
-            }
-
-            //  Append to slider
-            slidesContainer.appendChild(productCard);
+      // Cancel button
+      const cancelBtn = productCard.querySelector(".cancel-btn");
+      if (cancelBtn) {
+        cancelBtn.addEventListener("click", () => {
+          productCard.style.display = "none";
         });
-    }
+      }
+
+      //  Append to slider
+      slidesContainer.appendChild(productCard);
+    });
+  }
 }
 
 // close btn button
 function CLoseModalButton() {
-    // close modal
-    const closeModal = document.querySelector(".close-modal-btn");
-    const modal = document.querySelector("#subscribe-modal");
-    if (closeModal) {
-        closeModal.addEventListener("click", () => {
-            modal.style.display = "none";
-        });
-    }
+  // close modal
+  const closeModal = document.querySelector(".close-modal-btn");
+  const modal = document.querySelector("#subscribe-modal");
+  if (closeModal) {
+    closeModal.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  }
 }
 
 function handleModalPrimaryBtn() {
-    const data = window.abqarino_popup_var;
-    const btn = document.querySelector('.ok-modal-btn');
+  const data = window.abqarino_popup_var;
+  const btn = document.querySelector(".ok-modal-btn");
 
-    if (!btn || !data?.btn_link) return;
+  if (!btn || !data?.btn_link) return;
 
-    btn.addEventListener('click', () => {
-        window.location.href = data.btn_link;
-    });
+  btn.addEventListener("click", () => {
+    window.location.href = data.btn_link;
+  });
 }
 
-// promotion code 
+// promotion code
 function CopyPromotionCode() {
-    const copyBtn = document.getElementById("copyBtn");
-    const copyText = document.getElementById("copyText");
+  const copyBtn = document.getElementById("copyBtn");
+  const copyText = document.getElementById("copyText");
 
-    copyBtn.addEventListener("click", async () => {
-        try {
-            await navigator.clipboard.writeText(copyText.innerText);
+  copyBtn.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(copyText.innerText);
 
-            //  Toast Success
-            salla.notify.success("تم نسخ كود الخصم ✅");
+      //  Toast Success
+      salla.notify.success("تم نسخ كود الخصم ✅");
 
-            // Change button state
-            copyBtn.classList.add("active");
-            copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      // Change button state
+      copyBtn.classList.add("active");
+      copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
             </svg>`;
 
-            // Reset after 2 seconds
-            setTimeout(() => {
-                copyBtn.classList.remove("active");
-                copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      // Reset after 2 seconds
+      setTimeout(() => {
+        copyBtn.classList.remove("active");
+        copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
                     </svg>`;
-            }, 2000);
-
-        } catch (err) {
-
-            //Toast Error
-            salla.notify.error("فشل نسخ الكود ❌");
-        }
-    });
+      }, 2000);
+    } catch (err) {
+      //Toast Error
+      salla.notify.error("فشل نسخ الكود ❌");
+    }
+  });
 }
-
 
 // css style
 function injectStyle() {
-    const data = window.abqarino_popup_var;
-    const bgColor = data.bg_color;
+  const data = window.abqarino_popup_var;
+  const bgColor = data.bg_color;
 
-    const style = document.createElement("style");
+  const style = document.createElement("style");
 
-    style.innerHTML = `
+  style.innerHTML = `
         @media (max-width:768px){
             .modal-h{
                 max-height: 90vh;
@@ -553,5 +611,5 @@ function injectStyle() {
             }
     `;
 
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 }
